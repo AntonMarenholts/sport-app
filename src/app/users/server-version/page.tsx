48a759feeda58595c7aebb197982e078d296ee60
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-07-23 13:01:01 +0200

    add in server-version

--- src/app/users/server-version/page.tsx
+++ src/app/users/server-version/page.tsx
@@ -1,9 +1,32 @@
 import { User } from "@/types/types";
 
-
 // by default - server component
 const UsersServerVersion = async () => {
-  const res = await fetch("https://api.escuelajs.co/api/v1/users");
+  const res = await fetch("https://api.escuelajs.co/api/v1/users", {
+    headers: { "Api-Key": "afafafa.fafafafaf.agagag.9999900" },
+    cache: "no-store",
+  });
+
+  // кеширование с фетч - только на сервере
+
+  //                  1-в
+  // caсhe: "force-cash" - кеширование по умолчанию, дальше не обновляются
+  // используем : статические страницы(которые крайне редко меняются)
+
+  //                  2-в
+  // cache: "no-store" - не использует кэширование
+
+  //                  3-в
+  // Revalidate вариант :
+  // next: { revalidate: 60}, - запросит обновление страницы каждые (например 60 секунд)
+  // курсы валют, новости и т.д.
+
+  //                   4-в
+  // ручное обновление 
+  // next: { tags: [users]}, внутри фетча
+  // чтоб вызвать обновление мы можем 
+  // revalidateTag("users");
+
   if (!res.ok) {
     throw new Error("Fetch users failed");
   }
@@ -14,6 +37,8 @@ const UsersServerVersion = async () => {
       {users.map((user: User) => (
         <li key={user.id}>{user.name}</li>
       ))}
+
+      
     </div>
   );
 };
