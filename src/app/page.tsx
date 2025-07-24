e2a253c355349e5a6c51429bcaad83f648a36649
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-07-24 20:12:45 +0200

    add theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return <div className="p-8">
-    <p>Hello Next</p>
+    <p className="bg-background text-foreground">Hello Next</p>
+
+    <p className="bg-amber-400 dark:bg-cyan-700">Example</p>
     </div>;
 }
