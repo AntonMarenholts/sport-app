e1dc8677ac4affe3e47e9c537dfca17db65296b5
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-07-28 12:25:33 +0200

    add create product

---
+++ src/app/products/create/page.tsx
@@ -0,0 +1,35 @@
+import { createProduct } from "@/app/actions/createProduct";
+import { Category } from "@/types/types";
+
+export default async function page() {
+  const res = await fetch("https://api.escuelajs.co/api/v1/categories");
+  if (!res.ok) {
+    throw new Error("Failed to fetch categories");
+  }
+  const categories = await res.json();
+
+  return (
+    <div>
+      <form action={createProduct}>
+        <input type="text" name="title" placeholder="title" required />
+        <input type="number" name="price" placeholder="price" required />
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          required
+        />
+        <select name="categoryId" required>
+          {categories.map((category: Category) => (
+            <option value={category.id} key={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <input type="text" name="image" placeholder="image" required />
+
+        <button type="submit">Add products</button>
+      </form>
+    </div>
+  );
+}
