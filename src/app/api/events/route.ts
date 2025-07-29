4e43297161485318a403aa55e8f25328fc2aee43
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-07-29 11:58:51 +0200

    add in route

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,19 +1,36 @@
-import { db } from "@/db"
-import { eventsTable } from "@/db/schema"
+import { db } from "@/db";
+import { eventsTable } from "@/db/schema";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const EventInsertSchema = z.object({
+  name: z.string().min(3, "Minimal lenght is 3"),
+  description: z.string().max(250, "Description should be less then 250"),
+});
 
 export async function GET() {
-    const events = await db.select().from(eventsTable);
-    return NextResponse.json(events);
-    
+  const events = await db.select().from(eventsTable);
+  return NextResponse.json(events);
 }
 
-export async function POST(req:NextRequest){
+export async function POST(req: NextRequest) {
+  try {
     const body = await req.json();
+    const newEvent = EventInsertSchema.parse(body);
 
-    const [event] = await db.insert(eventsTable).values(body).returning();
+    const [event] = await db.insert(eventsTable).values(newEvent).returning();
 
-    
-
-    return NextResponse.json(event, {status:201});
+    return NextResponse.json(event, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: "Invalid input", details: error.issues },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
